1cb793e7c57b39313559e728b4b5bf3b1a4601d2
Ben Kulbertis <ben@kulbertis.org>
2011-06-25 11:19:21 -0400

    Added getUserLibrary() and switched to a function that decides on a return value for each function to avoid redundancy

--- GSAPI.php
+++ GSAPI.php
@@ -9,69 +9,81 @@
 
 // These functions added for php versions that don't include json functions
 function gs_json_decode($content, $arg) {
-    if (!extension_loaded('json')) {
-        if (!class_exists('GS_Services_JSON')) {
-            require_once 'GSJSON.php';
-        }
-        $json = new GS_Services_JSON(SERVICES_JSON_LOOSE_TYPE);
-        return $json->decode($content);
-
-    } else {
-        // just use php's json if it is available
-        return json_decode($content, $arg);
+  if (!extension_loaded('json')) {
+    if (!class_exists('GS_Services_JSON')) {
+      require_once 'GSJSON.php';
     }
+    $json = new GS_Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+    return $json->decode($content);
+  } else {
+    // just use php's json if it is available
+    return json_decode($content, $arg);
+  }
 }
 
 function gs_json_encode($content, $arg) {
-    if (!extension_loaded('json')) {
-        if (!class_exists('GS_Services_JSON')) {
-            require_once 'GSJSON.php';
-        }
-        $json = new GS_Services_JSON(SERVICES_JSON_LOOSE_TYPE);
-        return $json->encode($content);
-
-    } else {
-        // just use php's json if it is available
-        return json_encode($content, $arg);
+  if (!extension_loaded('json')) {
+    if (!class_exists('GS_Services_JSON')) {
+      require_once 'GSJSON.php';
     }
+    $json = new GS_Services_JSON(SERVICES_JSON_LOOSE_TYPE);
+    return $json->encode($content);
+  } else {
+    // just use php's json if it is available
+    return json_encode($content, $arg);
+  }
 }
 
 class GSAPI {
-    protected $auth = '';
-    protected $callCount = 0;
-		private static error = array();
-    private static $host = '';
-    private static $instance;
+  protected $auth = '';
+  protected $callCount = 0;
+  private static error = array();
+  private static $host = '';
+  private static $instance;
 
-		function __construct($host){
-			$this->host = $host;
-			$test = callRemote('');
-			if($test['Result']['code'] !== 202){
-				$this->host = '';
-				return "Invalid host/API key";
-			}
-		}
-    
-    private static function callRemote($method, $params = array()){
-        $url = 'http://'.self::$host.$method.'/'.self::format_params($params);
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
-        $result = curl_exec($curl);
-        curl_close($curl);
-				$decoded = gs_json_decode($result, true);
-				$this->callCount = $decoded["RateLimit"]["CallsRemaining"]; // Update CallCount on every request
-        return $decoded;
+  function __construct($host){
+    $this->host = $host;
+    /* Blank request to check if API key is valid. Wastes an API call, so only use if necessary.
+    $test = callRemote('');
+    if($test['Result']['code'] !== 202){
+      $this->host = '';
+      return "Invalid host/API key";
     }
+    */
+  }
     
-    private static function format_params($params){
-      $paramstring = '';
-      foreach ($params as $key => $value) {
-            $paramstring .= "$value/";
-      }
-      return $paramstring;
+  private static function callRemote($method, $params = array()){
+    $url = 'http://'.self::$host.$method.'/'.self::format_params($params);
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    $decoded = gs_json_decode($result, true);
+    $this->callCount = $decoded["RateLimit"]["CallsRemaining"]; // Update CallCount on every request
+    return $decoded;
+  }
+    
+  private static function format_params($params){
+    $paramstring = '';
+    foreach ($params as $key => $value) {
+      $paramstring .= "$value/";
     }
+    return $paramstring;
+  }
+  
+  private static function getReturn($result = array(), $bool = false){
+    if($result['Success'] != 1) {
+      $this->error = $result['Result'];
+      return false;
+    } else {
+      if($bool === false)
+        return $result['Result'];
+      else
+        return true;
+    }
+  }
 
     /*
      * Gets an instance of the GSAPI object
@@ -79,145 +91,121 @@ class GSAPI {
      * @param   string	String containing the API host URL
      * @return  GSAPI   Instance of the GSAPI object
      */
-    public static function getInstance($host){
-        if (!(self::$instance instanceof GSAPI)) {
-            self::$instance = new GSAPI($host);
-        }
-        return self::$instance;
+  public static function getInstance($host){
+    if (!(self::$instance instanceof GSAPI)) {
+      self::$instance = new GSAPI($host);
     }
+    return self::$instance;
+  }
 
-    public function getAuth() {return $this->auth;}
-    public function getApiCallsCount() {return $this->callCount;}
-		public function getError() {return $this->error;}
+  public function getAuth() {return $this->auth;}
+  public function getApiCallsCount() {return $this->callCount;}
+  public function getError() {return $this->error;}
 		
-		/**
-		 * Get information about the api key used.
-		 *
-		 * Does not count as an API call.
-		 */
-		public function APIKeyInfo(){
-			return self::callRemote('APIKeyInfo');
-		}
+  /**
+   * Get information about the api key used.
+   *
+   * Does not count as an API call.
+   */
+  public function APIKeyInfo(){
+    return self::callRemote('APIKeyInfo');
+  }
 		
-		/**
-		 * Add a song to a user's favorites. It is a violation to favorite songs for a user without notification.
-		 *
-		 * @param		int		songid
-		 */
-		public function addUserFavoriteSong($songid){
-			$result = self::callRemote('addUserFavoriteSong', array('GSAuth' => $this->auth, 'songID' => $songid));
-      if ($result['Success'] != 1) {
-					$this->error = $result['Result'];
-          return false;
-      } else {
-          return true;
-      }
-		}
+  /**
+   * Add a song to a user's favorites. It is a violation to favorite songs for a user without notification.
+   *
+   * @param		int		songid
+   */
+  public function addUserFavoriteSong($songid){
+    $result = self::callRemote('addUserFavoriteSong', array('GSAuth' => $this->auth, 'songID' => $songid));
+    return self::getReturn($result, true);
+  }
 
-		/**
-		 * Create a playlist.
-		 * 
-		 * @param		string	playlistName
-		 * @param		array		songids
-		 */
-		public function createPlaylist($playlistName, $songids = array()){
-			$songids = gs_json_encode($songids);
-			$result = self::callRemote('createPlaylist', array('GSAuth' => $this->auth, 'name' => $playlistName, 'songIDs' => $songids));
-      if ($result['Success'] != 1) {
-					$this->error = $result['Result'];
-          return false;
-      } else {
-          return true;
-      }
-		}
+  /**
+   * Create a playlist.
+   * 
+   * @param		string	playlistName
+   * @param		array		songids
+   */
+  public function createPlaylist($playlistName, $songids = array()){
+    $songids = gs_json_encode($songids);
+    $result = self::callRemote('createPlaylist', array('GSAuth' => $this->auth, 'name' => $playlistName, 'songIDs' => $songids));
+    return self::getReturn($result, true);
+  }
 
-    /**
-     * Authenticates the user for current API session
-		 * 
-		 * Accepts a username and password by default, but if $token is set to true it will accept
-		 * a pre-hashed token instead for security so that the password does not have to appear in plain text.
-     *
-     * @param   string  username
-     * @param   string  validation_string
-		 * @param		boolean	token
-     */
-    public function genGSAuth($username, $validation_string, $token = false){
-			if($token === false)	$validation_string = md5($username, md5($validation_string)); // APISHARK token format
-			$result = self::callRemote('genGSAuth', array('username' => $username, 'token' => $validation_string));
-      if ($result['Success'] != 1) {
-          return false;
-      } else {
-          $this->auth = $result['Result'];
-          return true;
-      }
-    }
+  /**
+   * Authenticates the user for current API session
+   * 
+   * Accepts a username and password by default, but if $token is set to true it will accept
+   * a pre-hashed token instead for security so that the password does not have to appear in plain text.
+   *
+   * @param   string  username
+   * @param   string  validation_string
+   * @param		boolean	token
+   */
+  public function genGSAuth($username, $validation_string, $token = false){
+    if($token === false)	$validation_string = md5($username, md5($validation_string)); // APISHARK token format
+    $result = self::callRemote('genGSAuth', array('username' => $username, 'token' => $validation_string));
+    return self::getReturn($result, true);
+  }
 
-		/**
-		 * Get a playlist's information and songs. If the playlist doesn't exist, no Name element will be returned.
-		 * 
-		 * @param		string	playlistID
-		 * 
-		 * @return	mixed		Playlist Information or false
-		 */
-		public function getPlaylistInfo($playlistID){
-			$result = self::callRemote('getPlaylistInfo', array('playlistID' => $playlistID));
-			if($result['Success'] != 1){
-				$this->error = $result['Result'];
-				return false;
-			} else {
-				return $result['Result'];
-			}
-		}
+  /**
+   * Get a playlist's information and songs. If the playlist doesn't exist, no Name element will be returned.
+   * 
+   * @param		string	playlistID
+   * 
+   * @return	mixed		Playlist Information or false
+   */
+  public function getPlaylistInfo($playlistID){
+    $result = self::callRemote('getPlaylistInfo', array('playlistID' => $playlistID));
+    return self::getReturn($result);
+  }
 
-    /**
-     * Gets song information
-     *
-     * @param   int     songID
-     * @return  mixed   Song information or error
-     */
-    public function getSongInfo($songID){
-        $result = self::callRemote('getSongInfo', array('songID' => $songID));
-        if (isset($result['Result'])) {
-            return $result['Result'];
-        } elseif (isset($result['errors'])) {
-            return array('error' => $result['errors'][0]['code']);
-        } else {
-            return array('error' => -8);
-        }
-    }
+  /**
+   * Gets song information
+   *
+   * @param   int     songID
+   * @return  mixed   Song information or false
+   */
+  public function getSongInfo($songID){
+    $result = self::callRemote('getSongInfo', array('songID' => $songID));
+    return self::getReturn($result);
+  }
 
-    /**
-     * Gets the favorite songs of the logged-in user
-     *
-     * @return  mixed   Songs list or error
-     */
-    public function userGetFavorites(){
-        if (empty($this->auth)) {
-            return array('error' => 'User Not Logged In');
-        }
-        $result = self::callRemote('getUserFavorites', array('GSAuth' => $this->auth));
-        
-        if($result['Success'] != 1 && isset($result['Result']['code'])){
-            return $result['Result']['code'].": ".$result['Result']['string'];
-        } else {
-            return $result['Result'];
-        }
-    }
+  /**
+   * Gets the favorite songs of the logged-in user
+   *
+   * @return  mixed   num array of songs or false
+   */
+  public function getUserFavorites(){
+    $result = self::callRemote('getUserFavorites', array('GSAuth' => $this->auth));
+    return self::getReturn($result);
+  }
+    
+  /**
+   * Gets the library of the logged-in user
+   *
+   * @return  mixed   num array of songs or false
+   */
+  public function getUserLibrary(){
+    $result = self::callRemote('getUserLibrary', array('GSAuth' => $this->auth));
+    return self::getReturn($result);
+  }
 
-    public function getWidgetEmbedCode($width, $height, $ids, $swfName = 'songWidget.swf', $ap = 0, $colors = null, $style = null){
-        $ids = 'songIDs=' . $ids['songIDs'];
-        $colors = is_array($colors) && !empty($colors) ? '&amp;' . http_build_query($colors) : '';
-        $ap = ($ap != 0) ? "&amp;p=$ap" : '';
-        $style = '&style=' . $style;
-        $embed = "
-        <object width='$width' height='$height'>
-            <param name='movie' value='http://listen.grooveshark.com/$swfName'></param>
-            <param name='wmode' value='window'></param>
-            <param name='allowScriptAccess' value='always'></param>
-            <param name='flashvars' value='hostname=cowbell.grooveshark.com&amp;{$ids}{$ap}{$colors}{$style}'></param>
-            <embed src='http://listen.grooveshark.com/$swfName' type='application/x-shockwave-flash' width='$width' height='$height' flashvars='hostname=cowbell.grooveshark.com&amp;{$ids}{$ap}{$colors}{$style}' allowScriptAccess='always' wmode='window'></embed>
-        </object>";
-        return $embed;
-    }
+  public function getWidgetEmbedCode($width, $height, $ids, $swfName = 'songWidget.swf', $ap = 0, $colors = null, $style = null){
+    $ids = 'songIDs=' . $ids['songIDs'];
+    $colors = is_array($colors) && !empty($colors) ? '&amp;' . http_build_query($colors) : '';
+    $ap = ($ap != 0) ? "&amp;p=$ap" : '';
+    $style = '&style=' . $style;
+    $embed = "
+    <object width='$width' height='$height'>
+        <param name='movie' value='http://listen.grooveshark.com/$swfName'></param>
+        <param name='wmode' value='window'></param>
+        <param name='allowScriptAccess' value='always'></param>
+        <param name='flashvars' value='hostname=cowbell.grooveshark.com&amp;{$ids}{$ap}{$colors}{$style}'></param>
+        <embed src='http://listen.grooveshark.com/$swfName' type='application/x-shockwave-flash' width='$width' height='$height' flashvars='hostname=cowbell.grooveshark.com&amp;{$ids}{$ap}{$colors}{$style}' allowScriptAccess='always' wmode='window'></embed>
+    </object>";
+    return $embed;
+  }
 
 }
